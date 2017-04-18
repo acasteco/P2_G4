1e0f2e1dbebb952adc4fca39498d600d1b00695a
alumneprova <alumneprova@S1-53>
2017-04-18 13:55:20 +0200

    All classes and Most of functions needed comented, implement.

---
+++ Alchemy/Main.cc
@@ -0,0 +1,24 @@
+/*
+class Elements{
+constructor: leer fichero "elements.data"(getline())) y los almacena en el unordered map (comprovar con un contador, que se han cargado todos los elementos, else System("pause"))
+unordered map usando los elementos que se combinan como key, y lo que obtenemos como value
+}
+class Player{
+
+public:
+(constructor, deve crear el vector con los elementos aire, fuego, tierra y agua)
+combine(elemento 1, elemento 2) -> elimina los 2 elementos y pone el resultado en la ultima posicion del vector. Y suma 1 a la puntuacion
+add() -> (copia del elemento)
+addBasics() -> (vuelve a añadir los elementos primarios)
+deleteElement() -> (borra elemento y posicion de memoriua que ocupaba)
+info(posicion elemento) -> abre pagina de wikipedia
+sort() -> se ordena el vector automaticamente
+clean() -> elimina los elementos repetidos
+help() -> muestra un tutorial con todas las acciones que puede hacer el jugador
+
+private:
+int puntuacion(+1 cada vez que creamos un elemento nuevo)
+vector para almacenar los elementos creados
+
+}
+*/
